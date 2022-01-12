fe67d9c839384da9959ca791b8e183f7d022609b
BenBreaksThings <78265023+BenBreaksThings@users.noreply.github.com>
2022-01-12 07:37:46 -0500

    Create Next_Round_158A.py

---
+++ Next_Round_158A.py
@@ -0,0 +1,8 @@
+# 158A - Next Round
+
+raw = input().split()
+n, k = int(raw[0]), int(raw[1])
+places = input().split()
+passing_val = int(places[k - 1])
+valid_places = [p for p in places if p != "0" and int(p) >= passing_val]
+print(str(len(valid_places)))
